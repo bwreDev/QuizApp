fc5d0ebb78f50c1ab69294c06d6aca6e689af0e8
Colton Bonner <cbonner26118@gmail.com>
2020-01-05 19:16:54 -0700

    added html and JS to project

---
+++ index.js
@@ -0,0 +1,37 @@
+const STATE = {
+    page: "#start-page",
+    currentQuestion: 0,
+    score: 0,
+    questions: [
+        {
+            title: "what is your name",
+            answers: ["Colton", "Mario", "John", "Dave"],
+            correct: 0,
+        }
+    ]
+};
+
+function render() {
+    $("section").hide();
+    $(STATE.page).show();
+    if (STATE.page == "#question-page") {
+        const question = STATE.questions[STATE.currentQuestion];
+        $("#labelOne").text(question.answers[0]);
+        //grab title question
+    }
+};
+
+function bindEvents() {
+    $("main").on("click", ".start-button", e => {
+        STATE.page = "#question-page";
+        render();
+    })
+};
+
+function main() {
+    bindEvents();
+    render();
+    console.log("main ran");
+}
+$(main);
+
