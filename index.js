389025f911c963358cae17618e7d389d3cb70481
Colton Bonner <cbonner26118@gmail.com>
2020-01-06 22:02:10 -0700

    Added better wireframing techniques to HTML, more syle choices, and questions added to JS file to clean up HTML page and still have access to questions

--- index.js
+++ index.js
@@ -5,10 +5,56 @@ const STATE = {
     score: 0,
     questions: [
         {
-            title: "what is your name",
-            answers: ["Colton", "Mario", "John", "Dave"],
+            title: "Who is the character that has to save Princess Peach from Bowser?",
+            answers: ["Luigi", "Mario", "Yoshi", "Link"],
+            correct: 1,
+        }
+        {
+            title: "Who is the character who must find the pieces of the TriForce in order to defeat Ganon?",
+            answers: ["Yoshi", "Zelda", "Link", "Mario"],
+            correct: 2,
+        }
+        {
+            title: "What game included the first ever \"easter egg\" in its code?",
+            answers: ["Asteroids", "Adventrue", "Yars Revenge", "Pong"],
+            correct: 1,
+        }
+        {
+            title: "Which video game series has you play as the character Master Chief?",
+            answers: ["Call of Duty", "Gears of War", "Battlefield", "Halo"],
+            correct: 3,
+        }
+        {
+            title: "In what major historical conflict, does the first several Call of Duty titles, take place in?",
+            answers: ["World War II", "World War I", "Vietnam War", "Gulf War"],
             correct: 0,
         }
+        {
+            title: "Which game console holds the exclusive rights for the racing simulator, Gran Turismo?",
+            answers: ["Xbox", "Dreamcast", "Playstation", "Nintendo"],
+            correct: 2,
+        }
+        {
+            title: "What competitive team based online game has you playing characters such as, Garen or Tristana or Miss Fortune?",
+            answers: ["Dota 2", "Heroes of the Storm", "Team Fortress 2", "League of Legends"],
+            correct: 3,
+        }
+        {
+            title: "What gametype is the best description for the game Skyrim?",
+            answers: ["Role Playing Game", "First Person Shooter", "Platformer", "Simulation"],
+            correct: 0,
+        }
+        {
+            title: "What fighting game features characters like Lou Kang, Johnny Cage, and Scorpion?",
+            answers: ["Streetfighter", "Soul Caliber", "Mortal Kombat", "Tekken"],
+            correct: 2,
+        }
+        {
+            title: "What popular car based game has you trying to score goals with an oversized soccer ball?",
+            answers: ["Forza", "Rocket League", "Mario Kart", "Need for Speed"],
+            correct: 1,
+        }
+
     ]
 };
 
