b5533f0410492ca1b3c374edadea33ce64b8940a
Colton Bonner <cbonner26118@gmail.com>
2020-01-07 23:09:39 -0700

    Added large amounts of JS functionality, tweaked HTML, heavily modified CSS

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* Worked with mentor on JS before turning in wireframe.
 const STATE = {
     page: "#start-page",
     currentQuestion: 0,
@@ -8,47 +7,47 @@ const STATE = {
             title: "Who is the character that has to save Princess Peach from Bowser?",
             answers: ["Luigi", "Mario", "Yoshi", "Link"],
             correct: 1,
-        }
+        },
         {
             title: "Who is the character who must find the pieces of the TriForce in order to defeat Ganon?",
             answers: ["Yoshi", "Zelda", "Link", "Mario"],
             correct: 2,
-        }
+        },
         {
             title: "What game included the first ever \"easter egg\" in its code?",
-            answers: ["Asteroids", "Adventrue", "Yars Revenge", "Pong"],
+            answers: ["Asteroids", "Adventure", "Yars Revenge", "Pong"],
             correct: 1,
-        }
+        },
         {
             title: "Which video game series has you play as the character Master Chief?",
             answers: ["Call of Duty", "Gears of War", "Battlefield", "Halo"],
             correct: 3,
-        }
+        },
         {
             title: "In what major historical conflict, does the first several Call of Duty titles, take place in?",
             answers: ["World War II", "World War I", "Vietnam War", "Gulf War"],
             correct: 0,
-        }
+        },
         {
             title: "Which game console holds the exclusive rights for the racing simulator, Gran Turismo?",
             answers: ["Xbox", "Dreamcast", "Playstation", "Nintendo"],
             correct: 2,
-        }
+        },
         {
             title: "What competitive team based online game has you playing characters such as, Garen or Tristana or Miss Fortune?",
             answers: ["Dota 2", "Heroes of the Storm", "Team Fortress 2", "League of Legends"],
             correct: 3,
-        }
+        },
         {
             title: "What gametype is the best description for the game Skyrim?",
             answers: ["Role Playing Game", "First Person Shooter", "Platformer", "Simulation"],
             correct: 0,
-        }
+        },
         {
             title: "What fighting game features characters like Lou Kang, Johnny Cage, and Scorpion?",
             answers: ["Streetfighter", "Soul Caliber", "Mortal Kombat", "Tekken"],
             correct: 2,
-        }
+        },
         {
             title: "What popular car based game has you trying to score goals with an oversized soccer ball?",
             answers: ["Forza", "Rocket League", "Mario Kart", "Need for Speed"],
@@ -58,19 +57,67 @@ const STATE = {
     ]
 };
 
+
 function render() {
     $("section").hide();
     $(STATE.page).show();
+    const question = STATE.questions[STATE.currentQuestion];
     if (STATE.page == "#question-page") {
-        const question = STATE.questions[STATE.currentQuestion];
+
         $("#labelOne").text(question.answers[0]);
-        //grab title question
+        $("#labelTwo").text(question.answers[1]);
+        $("#labelThree").text(question.answers[2]);
+        $("#labelFour").text(question.answers[3]);
+        $("#question-title").text(question.title);
     }
+    else if (STATE.page == "#feedback-page") {
+        if (STATE.correct) {
+            $(".correct-answer").show();
+            $(".wrong-answer").hide();
+        }
+        else {
+            let trueAnswer = question.answers[question.correct];
+            $(".trueAnswer").text(trueAnswer);
+            $(".wrong-answer").show();
+            $(".correct-answer").hide();
+        }
+    }
+    $('.questionNumberCounter').text(STATE.currentQuestion + 1);
+    $('.scoreCounter').text(STATE.score);
 };
+
 
 function bindEvents() {
     $("main").on("click", ".start-button", e => {
+        STATE.currentQuestion = 0;
+        STATE.score = 0;
         STATE.page = "#question-page";
+        render();
+    });
+    $("main").on("click", ".next-question-button", e => {
+        STATE.page = "#question-page";
+        STATE.currentQuestion++;
+        render();
+    });
+    $("main").on("submit", "form", e => {
+        e.preventDefault();
+        console.log(e.target.answer.value);
+        const question = STATE.questions[STATE.currentQuestion];
+        if (question.correct == e.target.answer.value) {
+            STATE.correct = true;
+            STATE.score++;
+        }
+        else {
+            STATE.correct = false;
+        }
+        if (STATE.currentQuestion < 9) {
+            STATE.page = "#feedback-page";
+
+        }
+        else {
+            STATE.page = "#score-page"
+        }
+        e.target.reset()
         render();
     })
 };
@@ -81,4 +128,3 @@ function main() {
     console.log("main ran");
 }
 $(main);
-*/
